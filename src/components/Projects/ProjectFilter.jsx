684e1bfdfba497c4a247c5ee8d046f21ca4f4837
Sabra Kozlina <sjkozlina@gmail.com>
2025-01-29 15:45:35 -0700

    updated CSS

--- src/components/Projects/ProjectFilter.jsx
+++ src/components/Projects/ProjectFilter.jsx
@@ -2,8 +2,8 @@ export const ProjectFilter = ({ allTypes, allLevels, setSelectType, setSelectLev
     return (
         <section className="project-filter">
             <div>
-                <label>Search Project Type: </label>
-                <select onChange={(event) => setSelectType(event.target.value)}>
+                <label className="info-title">Search Project Type: </label>
+                <select className="select-dropdown" onChange={(event) => setSelectType(event.target.value)}>
                     <option value="">Select Type</option>
                     {allTypes.map(type => {
                         return (<option value={type.id} key={type.id}>{type.name}</option>)
@@ -12,8 +12,8 @@ export const ProjectFilter = ({ allTypes, allLevels, setSelectType, setSelectLev
             </div>
 
             <div>
-                <label>Search Project Difficulty: </label>
-                <select onChange={(event) => setSelectLevel(event.target.value)}>
+                <label className="info-title">Search Project Difficulty: </label>
+                <select className="select-dropdown" onChange={(event) => setSelectLevel(event.target.value)}>
                     <option value="">Select Level</option>
                     {allLevels.map(level => {
                         return (<option value={level.id} key={level.id}>{level.name}</option>)
