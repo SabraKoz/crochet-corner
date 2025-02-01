b4114c847c9327d68c45bece0484b9a416d91fd5
Sabra Kozlina <sjkozlina@gmail.com>
2025-01-31 17:06:20 -0700

    tried to add background image but still need to get it to show up

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import { App } from "./App.jsx"
 import { BrowserRouter } from "react-router-dom"
 import { Theme, ThemePanel } from "@radix-ui/themes"
 import "@radix-ui/themes/styles.css"
+import "./main.css"
 
 const container = document.getElementById("root")
 const root = ReactDOM.createRoot(container);
 root.render(
   <BrowserRouter>
-    <Theme accentColor="violet" grayColor="mauve" panelBackground="translucent" >
+    <Theme accentColor="cyan" grayColor="mauve" panelBackground="translucent" >
       <App />
       <ThemePanel />
     </Theme>
